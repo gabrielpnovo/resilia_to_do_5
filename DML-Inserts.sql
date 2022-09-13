74d8714241f404db7bb7558b7fab660069a81696
gabrielpnovo <gabrielgn_@hotmail.com>
2022-09-13 07:57:41 -0300

    inserindo arquivos

---
+++ DML-Inserts.sql
@@ -0,0 +1,32 @@
+USE vitrine_virtual_todo5;
+
+INSERT INTO tbprodutos (
+NOME, DESCRICAO, ID_FORNECEDOR, CATEGORIA, TAMANHO, PRECO, SUSTENTAVEL, QTD_ESTOQUE) VALUES ('bermuda linho', 'bermuda em linho sustentável', 1, 'bermuda', 'M', 159.90, 1, 500);
+INSERT INTO tbprodutos (
+NOME, DESCRICAO, ID_FORNECEDOR, CATEGORIA, TAMANHO, PRECO, SUSTENTAVEL, QTD_ESTOQUE) VALUES ('short alfaiataria', 'short em alfaiataria', 2, 'short', 'P', 139.90, 0, 350);
+INSERT INTO tbprodutos (
+NOME, DESCRICAO, ID_FORNECEDOR, CATEGORIA, TAMANHO, PRECO, SUSTENTAVEL, QTD_ESTOQUE) VALUES ('camisa linho', 'camisa linho basica sustentavel', 3, 'camisa', 'G', 199.90, 1, 200);
+INSERT INTO tbprodutos (
+NOME, DESCRICAO, ID_FORNECEDOR, CATEGORIA, TAMANHO, PRECO, SUSTENTAVEL, QTD_ESTOQUE) VALUES ('bermuda linho', 'bermuda em linho sustentável', 4, 'bermuda', 'M', 159.90, 1, 500);
+INSERT INTO tbprodutos (
+NOME, DESCRICAO, ID_FORNECEDOR, CATEGORIA, TAMANHO, PRECO, SUSTENTAVEL, QTD_ESTOQUE) VALUES ('calca jogger', 'calca jogger em sarja sustentavel', 5, 'calca', 'P', 159.90, 1, 50);
+INSERT INTO tbprodutos (
+NOME, DESCRICAO, ID_FORNECEDOR, CATEGORIA, TAMANHO, PRECO, SUSTENTAVEL, QTD_ESTOQUE) VALUES ('calca cargo', 'calca cargo em sarja sustentabel', 6, 'calca', 'M', 179.90, 1, 480);
+INSERT INTO tbprodutos (
+NOME, DESCRICAO, ID_FORNECEDOR, CATEGORIA, TAMANHO, PRECO, SUSTENTAVEL, QTD_ESTOQUE) VALUES ('calca wide', 'calca wideleg em jeans sustentavel', 7, 'calca', 'GG', 259.90, 1, 360);
+INSERT INTO tbprodutos (
+NOME, DESCRICAO, ID_FORNECEDOR, CATEGORIA, TAMANHO, PRECO, SUSTENTAVEL, QTD_ESTOQUE) VALUES ('calca linho', 'calca linho basica sustentavel', 8, 'calca', 'PP', 299.90, 1, 200);
+INSERT INTO tbprodutos (
+NOME, DESCRICAO, ID_FORNECEDOR, CATEGORIA, TAMANHO, PRECO, SUSTENTAVEL, QTD_ESTOQUE) VALUES ('calca alfaiataria', 'calca em alfaiataria', 9, 'calca', 'P', 219.90, 0, 75);
+INSERT INTO tbprodutos (
+NOME, DESCRICAO, ID_FORNECEDOR, CATEGORIA, TAMANHO, PRECO, SUSTENTAVEL, QTD_ESTOQUE) VALUES ('bermuda jeans', 'bermuda jeans sustentavel', 10, 'bermuda', 'M', 159.90, 1, 360);
+INSERT INTO tbprodutos (
+NOME, DESCRICAO, ID_FORNECEDOR, CATEGORIA, TAMANHO, PRECO, SUSTENTAVEL, QTD_ESTOQUE) VALUES ('short jeans', 'short jeans sustentavel', 11, 'short', 'G', 139.90, 1, 600);
+INSERT INTO tbprodutos (
+NOME, DESCRICAO, ID_FORNECEDOR, CATEGORIA, TAMANHO, PRECO, SUSTENTAVEL, QTD_ESTOQUE) VALUES ('calca jeans', 'calca jeans basica sustentavel', 12, 'calca', 'PP', 179.90, 1, 560);
+INSERT INTO tbprodutos (
+NOME, DESCRICAO, ID_FORNECEDOR, CATEGORIA, TAMANHO, PRECO, SUSTENTAVEL, QTD_ESTOQUE) VALUES ('camisa estampada', 'camisa estampada', 13, 'camisa', 'M', 159.90, 0, 350);
+INSERT INTO tbprodutos (
+NOME, DESCRICAO, ID_FORNECEDOR, CATEGORIA, TAMANHO, PRECO, SUSTENTAVEL, QTD_ESTOQUE) VALUES ('camisa lisa', 'camisa lisa basica', 14, 'camisa', 'G', 129.90, 0, 400);
+INSERT INTO tbprodutos (
+NOME, DESCRICAO, ID_FORNECEDOR, CATEGORIA, TAMANHO, PRECO, SUSTENTAVEL, QTD_ESTOQUE) VALUES ('blusa sm', 'blusa sem manga', 15, 'blusa', 'P', 119.90, 0, 175);
---
+++ DML-Updates.sql
@@ -0,0 +1,7 @@
+USE vitrine_virtual_todo5;
+
+UPDATE tbprodutos SET PRECO = 209.90 WHERE ID_PRODUTO = 1;
+UPDATE tbprodutos SET TAMANHO = 'M' WHERE ID_PRODUTO = 3;
+UPDATE tbprodutos SET DESCRICAO = 'calca em alfaiataria sustentavel', SUSTENTAVEL = 1 WHERE ID_PRODUTO = 9;
+UPDATE tbprodutos SET TAMANHO = 'G' WHERE ID_PRODUTO = 13;
+UPDATE tbprodutos SET NOME = 'blusa sem manga', DESCRICAO = 'blusa sem manga basica' WHERE ID_PRODUTO = 15;
